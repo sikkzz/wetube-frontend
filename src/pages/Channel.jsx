00f32f7679477a2f2670bca1aa9ca5e963088f82
sik02 <pkwaw@naver.com>
2022-11-11 18:47:00 +0900

    channel 수정

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -3,6 +3,8 @@ import Header from '../components/Header/Header'
 import Sidebar from '../components/Sidebar/Sidebar'
 import ChannelContent from '../components/ChannelContent/ChannelContent'
 
+import './Channel.scss'
+
 const Channel = () => {
   return (
     <>
@@ -10,12 +12,12 @@ const Channel = () => {
         <div className="headers_container">
           <Header />
         </div>
-        <div className="contents_container">
-          <div className="sidebar_container">
+        <div className="channel_contents_container">
+          <div className="channel_sidebars_container">
             <Sidebar />
           </div>
-          <div className="mains_container">
-            <div className="detail_container">
+          <div className="channel_mains_container">
+            <div className="details_container">
               <ChannelContent />
             </div>
           </div>
