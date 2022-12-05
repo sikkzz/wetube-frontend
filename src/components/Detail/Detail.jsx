9d15a45228a52114623bfbb31a18e4ed9b0f0d9c
sling06 <pkwaw@naver.com>
2022-12-05 16:24:02 +0900

    detail페이지 구성

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,30 +1,24 @@
-import React from 'react'
-import Video from '../../assets/videos/test1.mp4'
-import './Detail.scss'
+import React from "react";
+import Video from "../../assets/videos/test1.mp4";
+import "./Detail.scss";
 
 const Detail = () => {
   return (
     <>
-    <div className="detail_containers">
-        <div className="detail_contents_container">
-          <div className="test">
-            <video
-              src={Video}
-              autoPlay
-              loop
-              controls
-              muted
-              type="video/mp4"
-              className="test_video"
-            />
+      <div className="detail_container">
+        <div className="detail_content_container">
+          <div className="detail_content_video_container">
+            <video src={Video} autoPlay loop controls muted type="video/mp4" />
           </div>
+
+          <div className="detail_content_info_container"></div>
+
+          <div className="detail_content_comment_container"></div>
         </div>
-        <div className='detail_playlist_container'>
-            
-        </div>
+        <div className="detail_playlist_container"></div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
