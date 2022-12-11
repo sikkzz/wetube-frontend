15d8f7eda85aad3b08d48ec7b2070507747e9e59
sling06 <pkwaw@naver.com>
2022-12-11 17:28:32 +0900

    [Modify] main layout grid css

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -6,7 +6,8 @@ import Video from "../../assets/videos/test.mp4";
 
 const Content = () => {
   const navigate = useNavigate();
-  const count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  const count = [1, 2, 3, 4];
+  const count2 = [1, 2, 3, 4];
 
   const onTest = () => {
     navigate("/upload");
@@ -18,59 +19,71 @@ const Content = () => {
 
   const handleOnMouseOut = (e) => {
     e.currentTarget.pause();
+<<<<<<< HEAD
     console.log("a+b+c")
+=======
+>>>>>>> 52e2696 ([Modify] main layout grid css)
   };
 
   return (
     <>
       <div className="content">
-        <div className="content_grid">
-          {count.map((index) => (
-            <div className="grid_container" key={index}>
-              <a href="/detail">
-                <div className="grid_card">
-                  <a href="/detail">
-                    <video
-                      loop
-                      muted
-                      onMouseOver={handleOnMouseOver}
-                      onMouseOut={handleOnMouseOut}
-                      className="content_profile_video"
-                      src={Video}
-                    />
-                  </a>
-                  <div className="content_detail_container">
-                    <a href="/channel" className="content_detail_profile_link">
-                      <div className="content_detail_profile_container">
-                        <img src={Image} alt="detail_profile_image" />
-                      </div>
-                    </a>
-                    <div className="content_detail_meta_container">
-                      <div className="content_detail_meta_title">
-                        <h3>
-                          <a href="/detail">
-                            <p>Test</p>
+        <div className="content_container">
+          <div className="content_grid_container">
+            {count2.map((index) => (
+              <div className="content_grid_row_container" key={index}>
+                {count.map((index) => (
+                  <div className="content_grid_row_item_container" key={index}>
+                    <a href="/detail">
+                      <div className="grid_card">
+                        <a href="/detail">
+                          <video
+                            loop
+                            muted
+                            onMouseOver={handleOnMouseOver}
+                            onMouseOut={handleOnMouseOut}
+                            className="content_profile_video"
+                            src={Video}
+                          />
+                        </a>
+                        <div className="content_detail_container">
+                          <a
+                            href="/channel"
+                            className="content_detail_profile_link"
+                          >
+                            <div className="content_detail_profile_container">
+                              <img src={Image} alt="detail_profile_image" />
+                            </div>
                           </a>
-                        </h3>
-                      </div>
+                          <div className="content_detail_meta_container">
+                            <div className="content_detail_meta_title">
+                              <h3>
+                                <a href="/detail">
+                                  <p>Test</p>
+                                </a>
+                              </h3>
+                            </div>
 
-                      <div className="content_detail_meta_body">
-                        <h3>
-                          <a href="/channel">김준식</a>
-                        </h3>
-                        <div className="content_detail_meta_body_data">
-                          <p>조회수 398만회</p>
-                          <p>1년전</p>
+                            <div className="content_detail_meta_body">
+                              <h3>
+                                <a href="/channel">김준식</a>
+                              </h3>
+                              <div className="content_detail_meta_body_data">
+                                <p>조회수 398만회</p>
+                                <p>1년전</p>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    </a>
                   </div>
-                </div>
-              </a>
-            </div>
-          ))}
-          <button onClick={onTest}>업로드 테스트</button>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
+        {/* <button onClick={onTest}>업로드 테스트</button> */}
       </div>
     </>
   );
