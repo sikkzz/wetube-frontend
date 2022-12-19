246b95b1a4a48a3d53d14ca717a61b8834cdcb3e
sling06 <pkwaw@naver.com>
2022-12-20 03:50:47 +0900

    [Test]: StudioContent 코드 삭제

--- src/pages/StudioPage/StudioPage.jsx
+++ src/pages/StudioPage/StudioPage.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import StudioHeader from "../../components/Header/StudioHeader";
 import StudioSidebar from "../../components/Sidebar/StudioSidebar";
-import StudioContent from "../../components/StudioContent/StudioContent";
-import Upload from "../../components/Upload/Upload";
 
 const StudioPage = () => {
   return (
     <>
       <StudioHeader />
-      {/* <StudioHeader /> */}
       <StudioSidebar />
-      <StudioContent />
-      {/* <wt-iron-overlay-backdrop class="opened" opened>
-        
-      </wt-iron-overlay-backdrop>
-      <Upload /> */}
     </>
   );
 };
