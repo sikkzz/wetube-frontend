2f778cf8172ddccf1f9756f062e301fae6688dea
sling06 <pkwaw@naver.com>
2022-12-20 02:08:01 +0900

    [Feat]: Studio header 재구성
     - StudioHeader.jsx: StudioHeader layout 수정
     - StudioHeader.scss: StudioHeader layout css 수정
     - HeaderSearchResults.jsx: Studio header search results component 추가
     - App.css: backdrop css 수정
     - common.scss: root css 추가

--- src/pages/StudioPage/StudioPage.jsx
+++ src/pages/StudioPage/StudioPage.jsx
@@ -4,16 +4,17 @@ import StudioSidebar from "../../components/Sidebar/StudioSidebar";
 import StudioContent from "../../components/StudioContent/StudioContent";
 import Upload from "../../components/Upload/Upload";
 
-
 const StudioPage = () => {
   return (
     <>
-      <wt-iron-overlay-backdrop class="opened" opened>
-        <StudioHeader />
-        <StudioSidebar />
-        <StudioContent />
+      <StudioHeader />
+      {/* <StudioHeader /> */}
+      <StudioSidebar />
+      <StudioContent />
+      {/* <wt-iron-overlay-backdrop class="opened" opened>
+        
       </wt-iron-overlay-backdrop>
-      <Upload />
+      <Upload /> */}
     </>
   );
 };
