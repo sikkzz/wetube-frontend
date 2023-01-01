072fc63e15ff71d157f253cae9c4544a5def6390
sling06 <pkwaw@naver.com>
2023-01-01 23:32:39 +0900

    Refactor: studio sidebar refactoring

--- src/pages/StudioPage/StudioPage.jsx
+++ src/pages/StudioPage/StudioPage.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import StudioHeader from "../../components/Header/StudioHeader";
 import StudioSidebar from "../../components/Sidebar/StudioSidebar";
-import StudioSidebar2 from "../../components/Sidebar/StudioSidebar2";
 
 const StudioPage = () => {
   return (
     <>
       <StudioHeader />
-      <StudioSidebar2 />
+      <StudioSidebar />
     </>
   );
 };
