ac1bc35c7d46740d7dbcd30361ec1825e4e59c18
sling06 <pkwaw@naver.com>
2023-01-07 15:18:57 +0900

    Refactor: sidebar layout refactoring

--- src/components/Sidebar/Sidebar2.jsx
+++ src/components/Sidebar/Sidebar2.jsx
@@ -5,6 +5,8 @@ import "./Sidebar2.scss";
 import Icons from "../../constants/icon";
 
 const Sidebar2 = () => {
+  const count = [1, 2, 3];
+  const count2 = [1, 2, 3, 4, 5];
   return (
     <div id="guide" className="wtd-app">
       <div id="guide-wrapper" className="wtd-app">
@@ -15,64 +17,53 @@ const Sidebar2 = () => {
               <div id="sections" className="wtd-guide-render">
                 <wtd-guide-section-render class="wtd-guide-render">
                   <div id="items" className="wtd-guide-section-render">
-                    <wtd-guide-entry-render class="wtd-guide-section-render">
-                      <a
-                        id="endpoint"
-                        className="wtd-guide-entry-render"
-                        href="/"
+                    {count.map((item, index) => (
+                      <wtd-guide-entry-render
+                        class="wtd-guide-section-render"
+                        key={index}
                       >
-                        <wt-paper-item class="wtd-guide-entry-render">
-                          <wt-icon class="guide-icon wtd-guide-entry-render">
-                            <Icons.AiOutlineHome size={24} color="#fff" />
-                          </wt-icon>
-                          <wt-formatted-string class="title wtd-guide-entry-render">
-                            홈
-                          </wt-formatted-string>
-                        </wt-paper-item>
-                      </a>
-                    </wtd-guide-entry-render>
-                    <wtd-guide-entry-render class="wtd-guide-section-render">
-                      <a
-                        id="endpoint"
-                        className="wtd-guide-entry-render"
-                        href="/"
-                      >
-                        <wt-paper-item class="wtd-guide-entry-render">
-                          <wt-icon class="guide-icon wtd-guide-entry-render">
-                            <Icons.AiOutlineHome size={24} color="#fff" />
-                          </wt-icon>
-                          <wt-formatted-string class="title wtd-guide-entry-render">
-                            홈
-                          </wt-formatted-string>
-                        </wt-paper-item>
-                      </a>
-                    </wtd-guide-entry-render>
-                    <wtd-guide-entry-render class="wtd-guide-section-render">
-                      <a
-                        id="endpoint"
-                        className="wtd-guide-entry-render"
-                        href="/"
-                      >
-                        <wt-paper-item class="wtd-guide-entry-render">
-                          <wt-icon class="guide-icon wtd-guide-entry-render">
-                            <Icons.AiOutlineHome size={24} color="#fff" />
-                          </wt-icon>
-                          <wt-formatted-string class="title wtd-guide-entry-render">
-                            홈
-                          </wt-formatted-string>
-                        </wt-paper-item>
-                      </a>
-                    </wtd-guide-entry-render>
+                        <a
+                          id="endpoint"
+                          className="wtd-guide-entry-render"
+                          href="/"
+                        >
+                          <wt-paper-item class="wtd-guide-entry-render">
+                            <wt-icon class="guide-icon wtd-guide-entry-render">
+                              <Icons.AiOutlineHome size={24} color="#fff" />
+                            </wt-icon>
+                            <wt-formatted-string class="title wtd-guide-entry-render">
+                              홈
+                            </wt-formatted-string>
+                          </wt-paper-item>
+                        </a>
+                      </wtd-guide-entry-render>
+                    ))}
 
                     <wtd-guide-section-entry-render class="wtd-guide-section-render">
-                      <div
+                      {/* <div
                         id="header"
                         className="wtd-guide-section-entry-render"
-                      >
-                        <wtd-guide-entry-render id="header-entry" class="wtd-guide-section-entry-render">
-                            
+                      > */}
+                      {count2.map((item, index) => (
+                        <wtd-guide-entry-render class="wtd-guide-section-render" key={index}>
+                          <a
+                            id="endpoint"
+                            className="wtd-guide-entry-render"
+                            href="/"
+                          >
+                            <wt-paper-item class="wtd-guide-entry-render">
+                              <wt-icon class="guide-icon wtd-guide-entry-render">
+                                <Icons.AiOutlineHome size={24} color="#fff" />
+                              </wt-icon>
+                              <wt-formatted-string class="title wtd-guide-entry-render">
+                                홈
+                              </wt-formatted-string>
+                            </wt-paper-item>
+                          </a>
                         </wtd-guide-entry-render>
-                      </div>
+                      ))}
+
+                      {/* </div> */}
                     </wtd-guide-section-entry-render>
                   </div>
                 </wtd-guide-section-render>
