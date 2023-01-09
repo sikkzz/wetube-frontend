a9bca431b9e335fc2c7fb8f752df16ad55e4c6b4
Kim JunSik <pkwaw@naver.com>
2023-01-09 21:49:19 +0900

    Refactor: channel layout refactoring

---
+++ src/components/ChannelContent/ChannelContent2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ChannelContent2.scss";
+import ChannelContentHeader from "./ChannelContentHeader";
+
+const ChannelContent2 = () => {
+  return (
+    <ChannelContentHeader />
+    
+  );
+};
+
+export default ChannelContent2;
