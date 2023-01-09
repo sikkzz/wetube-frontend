ed513b3371a668180a6b9b15f7bd51741b406720
Kim JunSik <pkwaw@naver.com>
2023-01-10 00:21:20 +0900

    Refactor: channel content layout refactoring

--- src/pages/ChannelPage/ChannelPage.jsx
+++ src/pages/ChannelPage/ChannelPage.jsx
@@ -3,14 +3,12 @@ import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import ChannelContent from "../../components/ChannelContent/ChannelContent";
 
-import ChannelContent2 from "../../components/ChannelContent/ChannelContent2";
-
 const ChannelPage = () => {
   return (
     <>
       <Header />
       <Sidebar />
-      <ChannelContent2 />
+      <ChannelContent />
     </>
   );
 };
